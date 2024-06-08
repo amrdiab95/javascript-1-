58a1003e9f07a7a35b99e0dabe72c520cd6ffe50
amrdiab95 <50382915+amrdiab95@users.noreply.github.com>
2024-06-08 16:27:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+// document.getElementById("march").innerHTML = "hello world from js file";
+
+
+// document.getElementById("march").style.textAlign="center";
+
+
+// document.getElementById("march").style.color="red";
+
+/* 
+askjfnaskjnfklasf
+skjdbfnkjsdnfklsdgf
+skjdbkjsdbgkjsdgv
+*/
